fcb8876bbb774d74f79a21f6fdf982b77e4cd03c
Mathieu Bret <mathieu.bret@ext.cdiscount.com>
2022-03-10 15:01:28 +0100

    feat: 🎸 Add beer icon

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+import logo from "./beer-svgrepo-com.svg";
 import "./App.css";
 
 function App() {
@@ -6,15 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to Chope My Beer ! The best app !</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Welcome to Chope My Beer</p>
       </header>
     </div>
   );
