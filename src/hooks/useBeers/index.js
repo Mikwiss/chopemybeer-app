2e99504eae5f3289caf04a4220b74e046f97ef91
Mathieu Bret <mathieu.bret@ext.cdiscount.com>
2022-03-16 11:56:49 +0100

    refactor: 💡 Use query-string in fetch module

--- src/hooks/useBeers/index.js
+++ src/hooks/useBeers/index.js
@@ -1,11 +1,14 @@
 import { useQuery } from "react-query";
+import queryString from "query-string";
 
 import { fetchPunkApi } from "./../../fetch";
 
 export default function useBeers() {
-  const { isLoading, error, data } = useQuery("beers", () =>
-    fetchPunkApi("beers")
-  );
+  const beersUrl = queryString.stringifyUrl({
+    url: "/beers",
+  });
+
+  const { isLoading, error, data } = useQuery("beers", fetchPunkApi(beersUrl));
 
   return { isLoading, error, beers: data };
 }
