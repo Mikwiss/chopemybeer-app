ef7abe7337dc9e7552170c2670f895b3ed0e19d5
Mathieu Bret <mathieu.bret@ext.cdiscount.com>
2022-03-16 10:30:11 +0100

    refactor: 💡 Clean useless files

--- src/Filter/index.js
+++
@@ -1,9 +0,0 @@
-import { FormControl, InputLabel } from "@mui/material";
-
-export default function Filter() {
-  return (
-    <FormControl>
-      <InputLabel>TODO</InputLabel>
-    </FormControl>
-  );
-}
